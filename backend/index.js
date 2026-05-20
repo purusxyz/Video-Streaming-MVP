3834d5835c09970f54fa709e24b7b344ae781f98
Purus <purusottampaswan925@gmail.com>
2026-05-21 00:11:40 +0530

    fix: correct FFmpeg command formatting and enable successful HLS conversion

--- backend/index.js
+++ backend/index.js
@@ -66,10 +66,7 @@ app.post("/upload", upload.single("file"), function(req, res){
     }
 
     //ffmpeg
-    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v
-    libx264 -codec:a aac -hls_time 10 -hls_playlist_type
-    vod -hls_segment_filename "${outputPath}/segment%03d.ts"
-    -start_number 0 ${hlsPath}`;
+    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
 
     // no queue because of MVP , not to be used in production
     exec(ffmpegCommand, (error, stdout, stderr) => {
