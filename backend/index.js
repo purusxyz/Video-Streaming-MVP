5dde6ca6bae49a47f5e2523b58a688337b54f800
Purus <purusottampaswan925@gmail.com>
2026-05-20 23:20:52 +0530

    add HLS video conversion and directory creation for uploads

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import multer from "multer"
 import {v4 as uuidv4 } from "uuid"
 import path from "path"
 import { log } from "console"
-import fs from "fs"
+import fs, { existsSync } from "fs"
 import { exec } from "child_process" // watch out
 
 
@@ -59,6 +59,33 @@ app.post("/upload", upload.single("file"), function(req, res){
     const outputPath = `./uploads/courses/${lessonId}`
     const hlsPath = `${outputPath}/index.m3u8`
     console.log("hlsPath", hlsPath)
+
+    if(!existsSync(outputPath)){
+        fs.mkdirSync(outputPath, {recursive: true})
+
+    }
+
+    //ffmpeg
+    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v
+    libx264 -codec:a aac -hls_time 10 -hls_playlist_type
+    vod -hls_segment_filename "${outputPath}/segment%03d.ts"
+    -start_number 0 ${hlsPath}`;
+
+    // no queue because of MVP , not to be used in production
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+        if (error){
+            console.log(`exec error: ${error}`)
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+        const videoUrl = `http://localhost:5000/upload/courses/${lessonId}/index.m3u8`
+
+        res.json({
+            message: "Video converted to HLS formate",
+            videoUrl: videoUrl,
+            lessonId: lessonId
+        })
+    })
    
 })
 
